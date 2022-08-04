27dee483e8daf5b3c97eaebe031f3597d0119735
anne <976413691@qq.com>
2022-08-04 21:52:04 +0800

    废弃的

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -2,20 +2,17 @@
  * @Author: anne.yang
  * @Date: 2022-07-29 11:39:07
  * @LastEditors: anne.yang
- * @LastEditTime: 2022-07-29 16:25:43
+ * @LastEditTime: 2022-07-30 16:19:34
  */
 // 创建“外壳”组件App
 import React, {Component} from "react";
-import Hello from "./components/Hello";
-import Welcome from "./components/Welcome";
 
 // 创建并暴露App组件
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Hello />
-                <Welcome />
+                <div>Hello, React!</div>
             </div>
         )
     }
---
+++ react_staging/src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: anne.yang
+ * @Date: 2022-07-30 16:34:57
+ * @LastEditors: anne.yang
+ * @LastEditTime: 2022-07-30 16:36:33
+ */
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
